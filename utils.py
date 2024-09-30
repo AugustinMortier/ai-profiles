2ee3d1cf1d429b9cea60f74a5f58a0e0bdd0b174
augustinm <augustinm@met.no>
2024-10-01 00:29:19 +0200

    wip

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import xarray as xr
 import matplotlib.pyplot as plt
 from rich.progress import track
 
-def snr(ds: xr.Dataset, variable, step=4, verbose=False) -> xr.Dataset:
+def snr(ds: xr.Dataset, variable, step=4, verbose=False, log=True) -> xr.Dataset:
     """
     Method that calculates the Signal to Noise Ratio.
     
@@ -25,7 +25,10 @@ def snr(ds: xr.Dataset, variable, step=4, verbose=False) -> xr.Dataset:
     
     for t in track(range(time_len), description="snr   ", disable=not verbose):
         # Extract 1D slice for current time step
-        array = ds[variable].data[t, :]
+        if log:
+            array = np.log10(ds[variable].data[t, :])
+        else:
+            array = ds[variable].data[t, :]
         
         # Create a sliding window view for the rolling calculation
         sliding_windows = np.lib.stride_tricks.sliding_window_view(array, window_shape=2*step+1, axis=0)
@@ -54,9 +57,40 @@ def snr(ds: xr.Dataset, variable, step=4, verbose=False) -> xr.Dataset:
     
     return ds
 
-def plot_pannel(ds: xr.Dataset, left, right):
-    # Create a figure with two subplots (1 row, 2 columns)
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+def gaussian_filter(
+        ds, sigma=0.25, var="attenuated_backscatter_0", inplace=False
+    ):
+        """
+        Applies a 2D gaussian filter in order to reduce high frequency noise.
+
+        Args:
+            sigma (scalar or sequence of scalars, optional): Standard deviation for Gaussian kernel. The standard deviations of the Gaussian filter are given for each axis as a sequence, or as a single number, in which case it is equal for all axes.
+            var (str, optional): variable name of the Dataset to be processed.
+            inplace (bool, optional): if True, replace the instance of the (ProfilesData): class.
+
+        Returns:
+            (xarray.DataArray).
+        """
+        import copy
+        from scipy.ndimage import gaussian_filter
+
+        # apply gaussian filter
+        filtered_data = gaussian_filter(ds[var].data, sigma=sigma)
+
+        if inplace:
+            ds[var].data = filtered_data
+            new_ds = ds
+        else:
+            copied_dataset = copy.deepcopy(ds)
+            copied_dataset[var].data = filtered_data
+            new_ds = copied_dataset
+        # add attribute
+        new_ds[var].attrs["gaussian_filter"] = sigma
+        return new_ds
+
+def plot_pannel(ds: xr.Dataset, left, middle, right):
+    # Create a figure with two subplots (1 row, 3 columns)
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
 
     # Plot attenuated_backscatter_0 on the first subplot
     ds[left['variable']].transpose().plot(
@@ -68,13 +102,22 @@ def plot_pannel(ds: xr.Dataset, left, right):
     axes[0].set_title(left['title'])
 
     # Plot snr on the second subplot
-    ds[right['variable']].transpose().plot(
+    ds[middle['variable']].transpose().plot(
         ax=axes[1], 
+        vmin=middle['vmin'], 
+        vmax=middle['vmax'], 
+        cmap=middle['cmap']
+    )
+    axes[1].set_title(middle['title'])
+
+    # Plot filter on the third subplot
+    ds[right['variable']].transpose().plot(
+        ax=axes[2], 
         vmin=right['vmin'], 
         vmax=right['vmax'], 
         cmap=right['cmap']
     )
-    axes[1].set_title(right['title'])
+    axes[2].set_title(right['title'])
 
     # Adjust layout for better spacing
     plt.tight_layout()
